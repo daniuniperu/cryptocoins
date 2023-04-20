2b0ad49e3cb292be67587858aed04fe67c905bec
Daniel Marcial Sosa Rios <dsosa@createthrive.com>
2023-04-20 16:03:06 -0500

    adding navbar

---
+++ src/routes/coins.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom"
+import "../styles/coins.css"
+
+const Coins =()=>{
+      return (
+            <div>
+                  AVAILABLE COINS
+            </div>
+      )
+}
+export default Coins;
